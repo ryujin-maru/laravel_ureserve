1d5b573a199a809b6cc576040298fc35263941cd
徳永龍仁 <ryujin-maru@ryujin.local>
2025-03-21 19:09:15 +0900

    event dummydate

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -3,7 +3,9 @@
 namespace App\Services;
 
 use App\Models\Event;
+use App\Models\Reservation;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class EventService
 {
@@ -28,5 +30,19 @@ class EventService
         ->whereTime('end_date','>',$start_time)
         ->whereTime('start_date','<',$end_time)
         ->count();
+    }
+
+    public static function getWeekEvents($startDate,$endDate)
+    {
+        $reservedPeople = Reservation::select('event_id',DB::raw('SUM(number_of_people) as number_of_people'))
+        ->whereNull('canceled_date')
+        ->groupBy('event_id');
+
+        return Event::leftJoinSub($reservedPeople,'reservedPeople',function($join) {
+            $join->on('events.id','=','reservedPeople.event_id');
+        })
+        ->whereBetween('start_date',[$startDate,$endDate])
+        ->orderBy('start_date','asc')
+        ->get();
     }
 }
